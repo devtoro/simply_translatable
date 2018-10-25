504b476ec3521acf15462d9e3b451aed3ad93f9e
Manolis <mtsilikidis@dialecticanet.com>
2018-10-25 17:46:24 +0300

    Init DB connection and test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+$LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
 require "simply_translatable"
-
 require "minitest/autorun"
+
+Dir[File.expand_path('../config/**/*.rb', __FILE__)].each { |f| require f }
+
+SimplyTranslatable::Test::Database.connect
