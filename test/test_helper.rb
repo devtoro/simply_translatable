c519efa72b1de418992b16d47f7dc2cfb92b90fd
Manolis <mtsilikidis@dialecticanet.com>
2018-10-25 18:40:28 +0300

    Set up test env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,14 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
 require "simply_translatable"
 require "minitest/autorun"
+require "minitest/reporters"
+require "minitest/spec"
+require 'database_cleaner'
 
+Minitest::Reporters.use!
 Dir[File.expand_path('../config/**/*.rb', __FILE__)].each { |f| require f }
+
+# Require all models create for testing
+# require File.expand_path('../data/models', __FILE__)
 
 SimplyTranslatable::Test::Database.connect
