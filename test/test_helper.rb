bd24036fdf1f5ac4bd63dd0792d5c11c454ce267
Manolis <mtsilikidis@dialecticanet.com>
2018-10-26 12:36:09 +0300

    Set up test env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,11 +4,44 @@ require "minitest/autorun"
 require "minitest/reporters"
 require "minitest/spec"
 require 'database_cleaner'
+require 'pry'
 
 Minitest::Reporters.use!
 Dir[File.expand_path('../config/**/*.rb', __FILE__)].each { |f| require f }
-
+Dir[File.expand_path('../data/models/*.rb', __FILE__)].each { |f| require f }
 # Require all models create for testing
 # require File.expand_path('../data/models', __FILE__)
 
+# binding.pry
 SimplyTranslatable::Test::Database.connect
+DatabaseCleaner.strategy = :transaction
+
+class MiniTest::Spec
+  before :each do
+    DatabaseCleaner.start
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
+
+  ActiveRecord::Base.class_eval do
+    class << self
+      def index_exists?(index_name)
+        connection.indexes(table_name).any? { |index| index.name == index_name.to_s }
+      rescue ActiveRecord::StatementInvalid
+        false
+      end
+
+      def index_exists_on?(column_name)
+        connection.indexes(table_name).any? { |index| index.columns == [column_name.to_s] }
+      rescue ActiveRecord::StatementInvalid
+        false
+      end
+
+      def unique_index_exists_on?(*columns)
+        connection.indexes(table_name).any? { |index| index.columns == columns && index.unique }
+      end
+    end
+  end
+end
