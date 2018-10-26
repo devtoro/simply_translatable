c15a94ea005d0a6c5ab94e1ee1a1a8d03e5baef5
Manolis <mtsilikidis@dialecticanet.com>
2018-10-26 22:03:35 +0300

    Pass test for postgres

--- test/simply_translatable_tests/page_test.rb
+++ test/simply_translatable_tests/page_test.rb
@@ -2,9 +2,18 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class AccessorsTest < MiniTest::Spec
   describe "New page creation" do
-    it "Has no title" do
+    it "New page has no title" do
       page = Page.new
       assert_equal nil, page.title
+    end
+  end
+
+  describe "Postgresql" do
+    # Available locales are [:en, :de]
+    it "Throws error if save locale other than I18n available locales" do
+      page = Page.new title: {ru: 'test'}
+
+      assert_equal false, page.valid?
     end
   end
 end
