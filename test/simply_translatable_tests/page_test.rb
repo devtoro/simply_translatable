21dd95b046d267265856a8dfd8f0f2b3dd4fb40e
Manolis <mtsilikidis@dialecticanet.com>
2018-10-26 22:52:10 +0300

    Test validations

--- test/simply_translatable_tests/page_test.rb
+++ test/simply_translatable_tests/page_test.rb
@@ -1,14 +1,16 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class AccessorsTest < MiniTest::Spec
-  describe "New page creation" do
-    it "New page has no title" do
-      page = Page.new
-      assert_equal nil, page.title
-    end
-  end
-
   describe "Postgresql" do
+    it "Has translatables data types set to be hstore" do
+      begin
+        page = Page.new rescue nil
+        assert_equal false, page.nil?
+      rescue ArgumentError
+        assert_equal false, true
+      end
+    end
+
     # Available locales are [:en, :de]
     it "Throws error if save locale other than I18n available locales" do
       page = Page.new title: {ru: 'test'}
