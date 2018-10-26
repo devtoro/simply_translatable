f9956574374676c82433a1ae4d826a25c6135bc1
Manolis <mtsilikidis@dialecticanet.com>
2018-10-26 22:03:20 +0300

    Add validation for locales

--- lib/simply_translatable.rb
+++ lib/simply_translatable.rb
@@ -3,10 +3,14 @@ require "simply_translatable/version"
 module SimplyTranslatable
   def self.included(klass)
     klass.extend(Translator)
+
+    klass.send(:validate, :validate_locales)
   end
+
 
   module Translator
     @@translatables = []
+
 
     def translates(*attributes)
       attributes.each do |att|
@@ -16,6 +20,16 @@ module SimplyTranslatable
 
     def get_translatables
       @@translatables
+    end
+  end
+
+  private
+
+  def validate_locales
+    self.class.get_translatables.each do |trans|
+      if (self.send(trans).keys & I18n.available_locales.map(&:to_s)).empty?
+        errors.add(trans, "Trying to save invalid locale. Please check I18n available locales.")
+      end
     end
   end
 end
