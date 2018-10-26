c5a00c01180101380fc454ea847cf2f892fb4094
Manolis <mtsilikidis@dialecticanet.com>
2018-10-26 23:38:07 +0300

    Add core functionality for Postgresql

--- lib/simply_translatable.rb
+++ lib/simply_translatable.rb
@@ -3,9 +3,20 @@ require "simply_translatable/version"
 module SimplyTranslatable
   def self.included(klass)
     klass.extend(Translator)
+    klass.after_initialize :set_translations
 
     klass.send(:validate, :validate_locales)
     klass.send(:before_save, :set_default_locales)
+  end
+
+  def method_missing(m, *args, &block)
+    attribute = m.to_s.gsub('translatable_', '').to_sym
+    if self.class.get_translatables.include?(attribute)
+      value = self.read_attribute attribute
+      value[I18n.locale.to_s] || value[I18n.locale]
+    else
+      super
+    end
   end
 
 
@@ -35,7 +46,7 @@ module SimplyTranslatable
 
   def validate_locales
     self.class.get_translatables.each do |trans|
-      if (self.send(trans).keys & I18n.available_locales.map(&:to_s)).empty?
+      if (self.read_attribute(trans).keys & I18n.available_locales.map(&:to_s)).empty?
         errors.add(trans, "Trying to save invalid locale. Please check I18n available locales.")
       end
     end
@@ -43,7 +54,27 @@ module SimplyTranslatable
 
   def set_default_locales
     self.class.get_translatables.each do |trans|
-      I18n.available_locales.each {|locale| self.send(trans)[locale.to_s] ||= ''}
+      I18n.available_locales.each {|locale| self.read_attribute(trans)[locale.to_s] ||= ''}
+    end
+  end
+
+  def set_translations
+    klass = self
+    self.class.get_translatables.each do |tr|
+      define_singleton_method tr do
+        klass.send("translatable_#{tr}")
+      end
+
+      define_singleton_method "#{tr}_translations" do
+        self.read_attribute tr
+      end
+
+      I18n.available_locales.each do |locale|
+        define_singleton_method "#{tr}_#{locale}" do
+          value = klass.read_attribute tr
+          value[locale.to_s] || value[locale]
+        end
+      end
     end
   end
 end
--- test/simply_translatable_tests/page_test.rb
+++ test/simply_translatable_tests/page_test.rb
@@ -21,7 +21,24 @@ class AccessorsTest < MiniTest::Spec
     it "Sets default locales as empty if none given" do
       page = Page.new title: {en: 'test'}
       page.save
-      assert_equal true, page.title.keys.include?('de')
+      assert_equal true, page.read_attribute("title").keys.include?('de')
+    end
+
+    it "Return default locale value from translations" do
+      page = Page.new title: {en: 'this is a test', de: 'bite'}
+      assert_equal page.title, 'this is a test'
+    end
+
+    it "Has method for each locale" do
+      page = Page.new title: {en: 'hello', de: 'bite'}
+      assert_equal page.title_en, 'hello'
+      assert_equal page.title_de, 'bite'
+    end
+
+    it "Has method to get all translations" do
+      page = Page.new title: {en: 'hello', de: 'bite'}
+      translations = page.title_translations
+      assert_equal translations, {"en" => 'hello', "de" => 'bite'}
     end
   end
 end
