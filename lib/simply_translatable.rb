0026e5c6840e1ad32a855187b10beb7aecf26cd3
Manolis <mtsilikidis@dialecticanet.com>
2018-10-28 11:10:28 +0200

    Fix adapter check for mysql

--- lib/simply_translatable.rb
+++ lib/simply_translatable.rb
@@ -28,8 +28,10 @@ module SimplyTranslatable
       adapter = self.connection.instance_variable_get('@config')[:adapter]
       attributes.each do |att|
         type = self.columns.select{|t| t.name==att.to_s}.first.sql_type_metadata.type
+        postgres = (adapter=="postgresql") && (type==:hstore)
+        mysql = (adapter=='mysql2') && (type==:json)
 
-        if (adapter=="postgresql") && (type==:hstore)
+        if postgres ^ mysql
           @@translatables << att
         else
           raise ArgumentError, "Translatable fields should be of type 'hstore', instead attribute #{att} has_type #{type}"
