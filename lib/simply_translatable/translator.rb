997a7ecc20f4b7b454fead7bdaac2497e3d76350
Manolis Tsilikidis <devtoro@users.noreply.github.com>
2018-11-05 20:44:28 +0200

    Update translator.rb

--- lib/simply_translatable/translator.rb
+++ lib/simply_translatable/translator.rb
@@ -1,33 +1,29 @@
 module SimplyTranslatable
   module Translator
-    @translatables = []
+    @@translatables = []
 
 
     def translates(*attributes)
       adapter = self.connection.instance_variable_get('@config')[:adapter]
-      attributes.each do |att|
-        type = self.columns.select{|t| t.name==att.to_s}.first.sql_type_metadata.type
-        postgres = (adapter=="postgresql") && (type==:hstore)
-        mysql = (adapter=='mysql2') && (type==:json)
+      type = self.columns.select{|t| t.name==att.to_s}.first.sql_type_metadata.type
+      postgres = (adapter=="postgresql") && (type==:hstore)
+      mysql = (adapter=='mysql2') && (type==:json)
 
-        if (postgres ^ mysql)
-          unless @translatables.include?(att)
-            @translatables << att
-          end
+      if (postgres ^ mysql)
+        @@translatables = attributes
+      else
+        if postgres
+          raise ArgumentError, "Translatable fields should be of type 'hstore', instead attribute #{att} has_type #{type}"
+        elsif mysql
+          raise ArgumentError, "Translatable fields should be of type 'text' or 'longtext', instead attribute #{att} has_type #{type}"
         else
-          if postgres
-            raise ArgumentError, "Translatable fields should be of type 'hstore', instead attribute #{att} has_type #{type}"
-          elsif mysql
-            raise ArgumentError, "Translatable fields should be of type 'text' or 'longtext', instead attribute #{att} has_type #{type}"
-          else
-            raise ArgumentError, "Adapter should be mysql2 or postgres"
-          end
+          raise ArgumentError, "Adapter should be mysql2 or postgres"
         end
       end
     end
 
     def get_translatables
-      @translatables
+      @@translatables
     end
   end
 end
